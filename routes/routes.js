aa8d47e9a86a761851efa4842e2f8c8088b812fe
Isaque Maestri <isaquemaestri01@gmail.com>
2020-06-12 17:14:09 -0300

    Refactoring the code. Using promises.

--- routes/routes.js
+++ routes/routes.js
@@ -1,177 +1,146 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
+const fs = require("fs").promises;
 
 
-router.post("/", (req, res) => {
-    let grade = req.body;
-    fs.readFile(global.fileName, "utf8", (error, data) =>{
-        try {
-            if(error) throw error;
+router.post("/", async (req, res) => {
+    try {
 
-            let jsonData = JSON.parse(data);
-            let time = new Date();
-            let timestamp = time.toLocaleString();
-            grade = {id: jsonData.nextId++,  ...grade, timestamp};
-            jsonData.grades.push(grade);
-    
-            fs.writeFile(global.fileName, JSON.stringify(jsonData), (error) =>{
-                if(error){
-                    console.log(error);
-                }else {
-                    res.end();
-                };
-            });
-        } catch (error) {
-            res.status(400).send({erorr: error.message});
-        }
-            
-    });
-});
-
-router.get("/:student/:subject", (req, res) => {
-   fs.readFile(global.fileName, "utf8", (error, data) => {
-       try {
-           if(error) throw error;
-
-           let jsonData  = JSON.parse(data);
-           const getStudent = jsonData.grades.filter(grade => grade.student === req.params.student && grade.subject === req.params.subject)
-           .reduce((accumulator, current) => {
-               return accumulator += current.value
-           }, 0);
-           let getStudentFormat  = JSON.stringify({total: getStudent});
-           if(getStudentFormat){
-               res.send(getStudentFormat);
-           }else{
-               res.end();
-           }
-       } catch (error) {
-        res.status(400).send({error: error.message});
-       }
-
-
-   });
-});
-
-
-router.get("/:subject/:type", (req, res) =>{
-    fs.readFile(global.fileName, "utf8", (error, data) => {
-        try {
-            if(error) throw error;
-
-            let jsonData = JSON.parse(data);
-            const getSubject = jsonData.grades.filter((grade) =>{
-                return grade.subject === req.params.subject && grade.type === req.params.type
-            });
-            const sizeArray = getSubject.length;
-            const reduceSubject =  getSubject.reduce((accumulator, current) =>{
-                return accumulator += current.value/sizeArray   
-            },0);
-           let getSubjectFormat = JSON.stringify({média: reduceSubject});
-           if(getSubjectFormat){
-               res.send(getSubjectFormat);
-           }else{
-               res.end();
-           }
-        } catch (error) {     
-            res.status(400).send({error: error.message});
-        }
-    });
-});
-
-router.get("/:subject/:type", (req, res) =>{
-    fs.readFile(global.fileName, "utf8", (error, data) =>{
-        try {
-            if(error) throw error;
-
-            let jsonData = JSON.parse(data);
-            const getSubject = jsonData.grades.filter((grade)=>{
-                return grade.subject === req.params.subject && grade.type === req.params.type
-            }).sort(function (a,b){
-                return b.value - a.value; 
-            }).slice(0,3);
-            console.log(getSubject);
-            let getSubjectFormat = JSON.stringify({top3: getSubject});
-            if(getSubjectFormat){
-                res.send(getSubjectFormat);
-            }else{
-                res.end();
-            }
-        } catch (error) {
-            res.status(400).send({error: error.message});
-        }
-
-
-    });
-});
-
-router.get("/:id", (req, res)=> {
-    fs.readFile(global.fileName, "utf8", (error, data) =>{
-        try {
-            if(error) throw error;
-
-            let jsonData = JSON.parse(data);
-            const grade = jsonData.grades.find(grade => grade.id === parseInt(req.params.id, 10));
-            if(grade){
-                res.send(grade);
-            }else{
-                res.end();
-            }
-        } catch (error) {
-            res.status(400).send({error: error.message});
-        }
-  
-    });
-});
-
-
-
-router.put("/", (req, res)=>{
-    let gradeUpdate = req.body;
-    fs.readFile(global.fileName, "utf8", (error, data) => {
-        try {
-            if(error) throw error;
-
-            let jsonData = JSON.parse(data);
-            let index = jsonData.grades.findIndex(grade => grade.id === gradeUpdate.id);
-            jsonData.grades[index] = gradeUpdate;
+        let grade = req.body;
+        let data = await fs.readFile(global.fileName, "utf8");
+        let jsonData = JSON.parse(data);
+        let time = new Date();
+        let timestamp = time.toLocaleString();
        
-            fs.writeFile(global.fileName, JSON.stringify(jsonData), (error) =>{
-               if(error){
-                   res.status(400).send({error: error.message});
-               }else {
-                   res.end();
-               };
-           });
-        } catch (error) {
-            res.status(400).send({error: error.message});
-        }
-    });
+        grade = { id: jsonData.nextId++, ...grade, timestamp };
+        jsonData.grades.push(grade);
+
+        await fs.writeFile(global.fileName, JSON.stringify(jsonData));
+        res.end();
+
+    } catch (error) {
+        res.status(400).send({ erorr: error.message });
+    }
+});
+
+router.get("/:student/:subject", async (req, res) => {
+    try {
+
+        let data = await fs.readFile(global.fileName, "utf8");
+        let jsonData = JSON.parse(data);
+        
+        const getStudent = jsonData.grades.filter((grade) => {
+           grade.student === req.params.student && grade.subject === req.params.subject
+        }).reduce((accumulator, current) => {
+            return accumulator += current.value
+        }, 0);
+        
+        let getStudentFormat = JSON.stringify({ total: getStudent });
+        res.send(getStudentFormat);
+        res.end();
+
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
+
+
+router.get("/:subject/:type", async (req, res) => {
+    try {
+
+        let data = await fs.readFile(global.fileName, "utf8");
+        let jsonData = JSON.parse(data);
+        
+        const getSubject = jsonData.grades.filter((grade) => {
+            return grade.subject === req.params.subject && grade.type === req.params.type
+        });
+        const sizeArray = getSubject.length;
+        const reduceSubject = getSubject.reduce((accumulator, current) => {
+            return accumulator += current.value / sizeArray
+        }, 0);
+        let getSubjectFormat = JSON.stringify({ média: reduceSubject });
+        
+        res.send(getSubjectFormat);
+        res.end();
+
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
+
+router.get("/:subject/:type", async (req, res) => {
+    try {
+
+        let data = await fs.readFile(global.fileName, "utf8");
+        let jsonData = JSON.parse(data);
+        
+        const getSubject = jsonData.grades.filter((grade) => {
+            return grade.subject === req.params.subject && grade.type === req.params.type
+        }).sort(function (a, b) {
+            return b.value - a.value;
+        }).slice(0, 3);
+        
+        let getSubjectFormat = JSON.stringify({ top3: getSubject });
+        res.send(getSubjectFormat);
+        res.end();
+
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+
+        let data = await fs.readFile(global.fileName, "utf8");
+        let jsonData = JSON.parse(data);
+        const grade = jsonData.grades.find(grade => grade.id === parseInt(req.params.id, 10));
+        
+        res.send(grade);
+        res.end();
+        
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 });
 
 
 
-router.delete("/:id", (req, res) => {
-    fs.readFile(global.fileName, "utf8", (error, data) =>{
-        try {
-            
-            if(error) throw error;
+router.put("/", async (req, res) => {
+    try {
+       
+        let gradeUpdate = req.body;
+        let data = await fs.readFile(global.fileName, "utf8");
+        let jsonData = JSON.parse(data);
+        let index = jsonData.grades.findIndex(grade => grade.id === gradeUpdate.id);
+        
+        jsonData.grades[index] = gradeUpdate;
 
-            let jsonData = JSON.parse(data);
-            const grade = jsonData.grades.filter(grade => grade.id !== parseInt(req.params.id, 10));
-            jsonData.grades = grade;
+        await fs.writeFile(global.fileName, JSON.stringify(jsonData));
+        res.end();
+    
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
+});
 
-            fs.writeFile(global.fileName, JSON.stringify(jsonData), (error) =>{
-                if(error){
-                    res.status(400).send({error: error.message});
-                }else{
-                    res.end();
-                }
-            }); 
-        } catch (error) {
-            res.status(400).send({error: error.message});  
-        }
-    });
+
+
+router.delete("/:id", async (req, res) => {
+    try {
+    
+        let data = await fs.readFile(global.fileName, "utf8");
+        let jsonData = JSON.parse(data);
+        const grade = jsonData.grades.filter(grade => grade.id !== parseInt(req.params.id, 10));
+        
+        jsonData.grades = grade;
+
+        await fs.writeFile(global.fileName, JSON.stringify(jsonData));
+        res.end();
+    
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 });
 
 // exportando os endpoints
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
+const fs = require("fs").promises;
 const gradesRouter = require("../routes/routes.js");
 
 global.fileName = "../files/grades.json";
@@ -8,21 +8,18 @@ global.fileName = "../files/grades.json";
 app.use(express.json());
 app.use("/grades", gradesRouter);
 
-app.listen(3000, function () {
-  try {
-    fs.readFile(global.fileName, "utf8", (error, data) => {
-      if (error) {
-        const initialJson = {
-          nextId: 1,
-          grades: [],
-        };
-        fs.writeFile(global.fileName, JSON.stringify(initialJson), (error) => {
-          if (error) {
-          }
-        });
-      }
-    });
-  } catch (error) {}
 
-  console.log("Api Started!");
+app.listen(3000, async () => {
+  try {
+    await fs.readFile(global.fileName, "utf8");
+    console.log("API Started!");
+  } catch (error) {
+    const initialJson = {
+      nextId: 1,
+      grades: [],
+    };
+    fs.writeFile(global.fileName, JSON.stringify(initialJson)).catch(error => {
+      console.log(error);
+    });
+  }
 });
